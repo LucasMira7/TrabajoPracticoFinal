57c697bec4c8283e018cdebd1c610e49456b76ed
LucasMira7 <lucasgastonmira@gmail.com>
2023-06-15 01:11:27 -0300

    cambios

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+hola
+// variable = click en botonplay
+// if variable == botonplay1{tal cosa}
+// else if variable == botonplay2{tal cosa}
+//     if variable == botonplay3{tal cosa}
+//     else if variable == botonplay4{tal cosa}
+//         if variable == botonplay5{tal cosa}
+//         else if variable == botonplay6{tal cosa}
+//             if variable == botonplay7{tal cosa}
+//             else if variable == botonplay8{tal cosa}
+//                 if variable == botonplay9{tal cosa}
+//                 else if variable == botonplay2{tal cosa}
+//                     if variable == botonplay10{tal cosa}
+//                     else if variable == botonplay11{tal cosa}
+//                         if variable == botonplay12{tal cosa}
+//                         else if variable == botonplay13{tal cosa}
+//                             if variable == botonplay14{tal cosa}
+//                             else if variable == botonplay15{tal cosa}
